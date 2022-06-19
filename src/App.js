1bf0d5212dc8c4eb338c7ce50e3ea6128db0db2a
zainaboyedeji <ololadeoyedejiii@gmail.com>
2022-06-19 16:45:55 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.scss';
 import MainView from './Pages/MainView/index';
+import ReactGA from 'react-ga';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from "react-router-dom";
 
+const TRACKING_ID = "G-TVH8XXJ6F1"git ; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
