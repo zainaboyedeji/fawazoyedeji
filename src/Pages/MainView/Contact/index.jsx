77f8aede766690a11a4bf31c18b1bf41bc9d0557
zainab8372341 <zainab@uniswitchng.com>
2023-11-06 22:13:18 +0100

    worked on contact page

--- src/Pages/MainView/Contact/index.jsx
+++ src/Pages/MainView/Contact/index.jsx
@@ -4,12 +4,11 @@ import MobileHead from "../../../Components/MobileHead";
 function Contact() {
   return (
     <div className="contact">
-      <MobileHead/>
+      <MobileHead />
       <div className="line"></div>
       <div className="arr">
         <p>
-          For inquiries regarding availability for commissions, image licensing,
-          and print purchase, please contact.
+          For inquiries regarding availability for commissions, image licensing, and print purchase, please contact.
         </p>
       </div>
 
@@ -17,9 +16,9 @@ function Contact() {
         <h1>FAWAZ OYEDEJI.</h1>
       </div>
       <div className="arr">
-        <p>email: <a href="mailto:fawazoyedeji01@gmail.com">fawazoyedeji01@gmail.com</a></p>
+        <p>email: <a href="mailto:contactfawazoyedeji@gmail.com">contactfawazoyedeji@gmail.com</a></p>
 
-        <p>tel: +234-90-93173219</p>
+        <p>whatsapp: +234-90-93173219</p>
       </div>
     </div>
   );
