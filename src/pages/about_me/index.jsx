222b0d25bb9bb6b939b781b1ca5c9839d783a1ec
Zainab Oyedeji <zainab@uniswitchng.com>
2024-09-12 12:04:56 +0100

    new changes

--- src/pages/about_me/index.jsx
+++ src/pages/about_me/index.jsx
@@ -29,38 +29,37 @@ function AboutMe() {
               power, understanding it not just as a repressive force but as one
               that operates actively within various levels of social structures,
               such as political authority, social class, institutions, and
-              events. 
+              events.
             </p>
 
             <p className="mb-5">
-              He holds a background in 
-              <span className="bold-text">
-                Mass Communication from Yaba College of Technology
-              </span> 
-              and studied 
-              <span className="bold-text">
+              He holds a background in  
+              <span className="bold-text ml-1 mr-1">
+                 Mass Communication from Yaba College of Technology
+              </span>
+              and studied
+              <span className="ml-1 mr-1 bold-text">
                 History Education at the University of Lagos
               </span>
-              . In 2023, he graduated from the 
-              <span className="bold-text">
+              . In 2023, he graduated from the
+              <span className="bold-text ml-1 mr-1">
                 Market Photo Workshop in South Africa,
-              </span> 
-              further refining his photojournalism and documentary practice. 
+              </span>
+              further refining his photojournalism and documentary practice.
             </p>
 
             <p className="mb-5">
               Fawaz’s work, which covers themes such as the rise of private
               security companies, the role of political posters during
               elections, protests against police violence, and the lives of
-              student cadets, has been published in 
-              <span className="bold-text">The Financial Times, Les Echos </span> 
+              student cadets, has been published in
+              <span className="bold-text"> The Financial Times, Les Echos </span>
               and exhibited globally, earning him grants, awards, and
               commissions from prestigious organizations like the 
-              <span className="bold-text">
-                 
-                W. Eugene Smith Fund, Prince Claus Fund, Christian Dior Parfums,
+              <span className="bold-text ml-1 mr-1">
+                 W. Eugene Smith Fund, Prince Claus Fund, Christian Dior Parfums,
                 Daniele Tamagni Foundation, UN WFP, Architectural Review, Magnum
-                Photos, and Agence France-Presse (AFP), 
+                Photos, and Agence France-Presse (AFP),
               </span>
               etc.
             </p>
